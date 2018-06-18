2277c1c38070a39cef6e2d4e18fd78a6fdf267a8
TheColucciExperience <thecolucciexperience@outlook.com>
2018-06-18 00:23:53 -0300

    Initial Setup Ready

---
+++ src/main.js
@@ -0,0 +1,9 @@
+
+// Main script for importing assets, modules, etc.
+
+import './index.html'
+import './scss/styles.scss';
+
+window.addEventListener( 'load', function someAction() {  
+  console.log( 'ok' );
+} );
